a0b6d0be42d98e87f92fd36a794336aaef98ff67
Abhiraj0406 <abhirajvishwakarma04@gmail.com>
2026-07-09 15:13:51 +0530

    issue: add AuthorizesRequests trait to base controller to enable authorization support

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 abstract class Controller
 {
-    //
+    // In Laravel 12, this trait is NOT included by default.
+    // We add it manually so that $this->authorize() works in all controllers.
+    // $this->authorize('create', ShortUrl::class) → checks ShortUrlPolicy::create()
+    use AuthorizesRequests;
 }
